98cbba7fd0ffe321a4c71b75d4355d6b824eeba3
anwin-kp <anwin.kp@gadgeon.com>
2023-07-17 13:19:49 +0530

    api call added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 import 'dart:isolate';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -43,9 +46,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // Replace this with your own logic if needed.
     // For demonstration purposes, we'll send the counter value every 1 second.
     int counter = 0;
-    Timer.periodic(const Duration(seconds: 1), (timer) {
+    Timer.periodic(const Duration(seconds: 5), (timer) {
       counter++;
       port.send(counter);
+      loginApiCall();
     });
   }
 
@@ -76,5 +80,54 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ),
     );
+  }
+}
+
+Future<bool> loginApiCall() async {
+  late http.Response response;
+  try {
+    response = await http.get(
+      Uri.parse('https://reqres.in/api/users/2'),
+      headers: <String, String>{
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    );
+  } on Exception catch (e) {
+    if (e is SocketException) {
+      if (kDebugMode) {
+        print('Failed to connect to the host');
+      }
+    } else if (e is FormatException) {
+      if (kDebugMode) {
+        print('Failed to connect to the host');
+      }
+    } else if (response.statusCode == 500) {}
+  }
+  if (response.statusCode == 200) {
+    String jsonResponse = response.body;
+    // Parse the JSON response
+    Map<String, dynamic> responseData = jsonDecode(jsonResponse);
+
+    // Extract the email
+    String email = responseData['data']['email'];
+
+    if (kDebugMode) {
+      print(
+          "API call Success(Response Code - ${response.statusCode}) -> $email");
+    }
+  } else {
+    var messages = jsonDecode(response.body);
+    Map<String, dynamic> value = messages;
+    String element = value.values.elementAt(0);
+    if (kDebugMode) {
+      print(element);
+    }
+  }
+  var result = response.body;
+
+  if (response.statusCode == 200 && result.runtimeType == String) {
+    return true;
+  } else {
+    return false;
   }
 }
